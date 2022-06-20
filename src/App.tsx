ec4d06cd1589c91ff267548c70a6230f0140ba7f
Jason Wang <jhw513@gmail.com>
2022-06-20 14:56:50 -0700

    technically this works

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,54 @@
-import React, { FunctionComponent, useState } from "react";
-import logo from "./logo.svg";
+import React, { FC, useState } from "react";
+import headshot from "./assets/headshot.jpg";
 import "./App.css";
 
-const App: FunctionComponent = () => {
+type Props = {
+  children?: React.ReactNode;
+};
+
+const Greeting: FC<Props> = ({ children }) => (
+  <div tw="border hover:border-black">{children}</div>
+);
+
+const Paragraph: FC<Props> = ({ children }) => (
+  <div tw="border hover:border-black">{children}</div>
+);
+
+const Link: FC<Props & { link: string }> = ({ children, link }) => (
+  <span tw="cursor-pointer" onClick={() => window.open(link)}>
+    {children}
+  </span>
+);
+
+const App: FC = () => {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>Hello Vite + React!</p>
-      <p>
-        <button type="button" onClick={() => setCount((count) => count + 1)}>
-          count is: {count}
-        </button>
-      </p>
-      <p>
-        Edit <code>App.tsx</code> and save to test HMR updates.
-      </p>
+      <img src={headshot} />
+      <Greeting>Hi, I'm Jason</Greeting>
+      <Paragraph>
+        I'm currently a software engineer at{" "}
+        <Link link="https://getcensus.com">Census</Link>, helping teams use data
+        in their day-to-day operations. Previously, I cofounded{" "}
+        <Link link="https://cohere.io">Cohere</Link>, and was at{" "}
+        <Link link="https://about.fb.com/news/2018/06/all-of-your-facebook-memories-are-now-in-one-place/">
+          Facebook
+        </Link>
+        ,{" "}
+        <Link link="https://www.plm.automation.siemens.com/">Siemens R&D</Link>,{" "}
+        and{" "}
+        <Link link="https://www.bloomberg.com/news/articles/2020-08-03/farmers-business-network-raises-funds-at-1-75-billion-valuation">
+          Farmers Business Network
+        </Link>
+        .
+      </Paragraph>
+      <Paragraph>
+        You can find me on{" "}
+        <Link link="https://github.com/kamesstory">Github</Link>,{" "}
+        <Link link="https://linkedin.com/in/jason-hf-wang">LinkedIn</Link>, or
+        through my <Link link="mailto:jhw513@gmail.com">email</Link>.
+      </Paragraph>
     </>
   );
 };
